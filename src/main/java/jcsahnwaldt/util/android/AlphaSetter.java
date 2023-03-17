513dbdc7185171e85883c6415ab8d0c0598e478b
Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2023-03-17 22:45:49 +0100

    AlphaSetter.java

---
+++ src/main/java/jcsahnwaldt/util/android/AlphaSetter.java
@@ -0,0 +1,32 @@
+package jcsahnwaldt.util.android;
+
+import android.os.Build;
+import android.view.View;
+import android.widget.ImageView;
+
+public class AlphaSetter
+extends SubViewModifier
+{
+  private final float alpha;
+
+  public AlphaSetter(float alpha) {
+    this(0, alpha);
+  }
+
+  public AlphaSetter(int viewId, float alpha) {
+    super(viewId);
+    this.alpha = alpha;
+  }
+
+  @Override
+  public boolean canUse(View view) {
+    return findView(view, false) instanceof ImageView;
+  }
+
+  @Override
+  public void modify(View view) {
+    ImageView imageView = (ImageView)findView(view, true);
+    if (Build.VERSION.SDK_INT >= 11) imageView.setAlpha(alpha);
+    else imageView.setAlpha((int)(alpha * 255));
+  }
+}
