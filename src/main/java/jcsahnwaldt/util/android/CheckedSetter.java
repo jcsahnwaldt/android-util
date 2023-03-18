c912677c1f0113bedbbc289215a132dbeeb8afb6
Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2023-03-18 17:29:28 +0100

    CheckedSetter.java

---
+++ src/main/java/jcsahnwaldt/util/android/CheckedSetter.java
@@ -0,0 +1,30 @@
+package jcsahnwaldt.util.android;
+
+import android.view.View;
+import android.widget.Checkable;
+
+public class CheckedSetter
+extends SubViewModifier
+{
+  private final boolean checked;
+
+  public CheckedSetter(boolean checked) {
+    this(0, checked);
+  }
+
+  public CheckedSetter(int viewId, boolean checked) {
+    super(viewId);
+    this.checked = checked;
+  }
+
+  @Override
+  public boolean canUse(View view) {
+    return findView(view, false) instanceof Checkable;
+  }
+
+  @Override
+  public void modify(View view) {
+    Checkable checkable = (Checkable)findView(view, true);
+    checkable.setChecked(checked);
+  }
+}
