b75ae2f1022bd2d3c27e0da8be71aba0b7ccf025
Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2023-05-03 13:07:46 +0200

    ScaledTypeface.java: remove name and toString()

--- src/main/java/jcsahnwaldt/android/ScaledTypeface.java
+++ src/main/java/jcsahnwaldt/android/ScaledTypeface.java
@@ -7,12 +7,10 @@ import android.widget.TextView;
 
 public class ScaledTypeface {
 
-  public static ScaledTypeface createFromAsset(AssetManager mgr, String file, String name, float factor, int tagKey) {
-    if (file == null) return new ScaledTypeface(Typeface.DEFAULT, name, factor, tagKey);
-    else return new ScaledTypeface(Typeface.createFromAsset(mgr, file), name, factor, tagKey);
+  public static ScaledTypeface createFromAsset(AssetManager mgr, String file, float factor, int tagKey) {
+    if (file == null) return new ScaledTypeface(Typeface.DEFAULT, factor, tagKey);
+    else return new ScaledTypeface(Typeface.createFromAsset(mgr, file), factor, tagKey);
   }
-
-  private final String name;
 
   private final Typeface typeface;
 
@@ -24,14 +22,12 @@ public class ScaledTypeface {
    */
   private final int tagKey;
 
-  public ScaledTypeface(Typeface typeface, String name, float factor, int tagKey) {
-    if (name == null) throw new NullPointerException("name");
+  public ScaledTypeface(Typeface typeface, float factor, int tagKey) {
     if (typeface == null) throw new NullPointerException("typeface");
     if (factor <= 0.0f) throw new IllegalArgumentException("need positive factor, got "+factor);
     // same check as in View.setTag(int, Object)
     if ((tagKey >>> 24) < 2) throw new IllegalArgumentException("tag key must be application-specific resource id, got 0x"+Integer.toHexString(tagKey));
 
-    this.name = name;
     this.typeface = typeface;
     this.factor = factor;
     this.tagKey = tagKey;
@@ -46,10 +42,4 @@ public class ScaledTypeface {
     }
     textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, size*factor);
   }
-
-  @Override
-  public String toString() {
-    return name+", factor "+factor;
-  }
-
 }
